78d4a37e3c2ef56a7ae14d960bb8f69be2806101
Ippei Tanaka <itanaka@unioncreative.com>
2016-06-17 02:07:27 -0400

    Add argument checker to SchemaData

--- src/schema-data.js
+++ src/schema-data.js
@@ -1,30 +1,56 @@
+import Immutable from 'immutable';
+import { SimpleOdmError } from './errors';
+
 export default class SchemaData {
 
     /**
-     * @param values {Immutable.Map}
-     * @param errorMessages {Immutable.Map}
+     * @param values {Immutable.Map.<*>}
+     * @param errorMessages {Immutable.Map.<Immutable.List.<string>>}
      */
-    constructor({values, errorMessages})
+    constructor ({values, errorMessages})
     {
+        if (!(errorMessages instanceof Immutable.Map))
+        {
+            throw new SimpleOdmError("The errorMessages argument has to be Immutable.Map");
+        }
+
+        errorMessages.forEach((messageList) =>
+        {
+            if (!(messageList instanceof Immutable.List))
+            {
+                throw new SimpleOdmError("Each value of errorMessages argument has to be Immutable.List");
+            }
+
+            messageList.forEach((message) =>
+            {
+                if (typeof message !== 'string')
+                {
+                    throw new SimpleOdmError("Each value of errorMessages argument List has to be string");
+                }
+            })
+        });
+
         this._values = values;
         this._errorMessages = errorMessages;
         Object.freeze(this);
     }
 
-    get values()
+    get values ()
     {
         return this._values;
     }
 
-    get errorMessages()
+    get errorMessages ()
     {
         return this._errorMessages;
     }
 
-    get hasErrors()
+    get hasErrors ()
     {
-        for (let messages of this._errorMessages.values()) {
-            if (messages.size > 0) {
+        for (let messages of this._errorMessages.values())
+        {
+            if (messages.size > 0)
+            {
                 return true;
             }
         }
