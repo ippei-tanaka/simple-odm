f8fbec6d02453bd930891b0119e8ab2dd32cb3af
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2016-06-24 21:52:07 -0400

    Update mongo-utils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 export { default as MongoDriver } from './mongo-driver';
 export { default as MongoUtils } from './mongo-utils';
+export { default as MongoUtilsBuilder } from './mongo-utils-builder';
 export { default as MongoModelBuilder } from './mongo-model-builder';
 export { default as MongoSchema } from './mongo-schema';
 export { default as Type } from './type';
+export { default as EventHub } from './event-hub';
