cc9b50eb4abb16389ff62ffd94287888a2a6344f
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2016-06-24 21:28:56 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-export { default as ModelBuilder } from './model-builder';
-export { default as MongoCrudOperator } from './mongo-crud-operator';
 export { default as MongoDriver } from './mongo-driver';
-export { default as MongoUtils } from './mongo-utils';
+export { default as MongoModelBuilder } from './mongo-model-builder';
 export { default as Schema } from './schema';
 export { default as Type } from './type';
